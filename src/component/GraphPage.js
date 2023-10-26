40dd1da2ffa0b79fa8fe2b478a596dbe25ba2ae6
Laraib Ahmad <110982212+labbucode@users.noreply.github.com>
2023-10-26 19:57:22 +0530

    Update GraphPage.js

--- src/component/GraphPage.js
+++ src/component/GraphPage.js
@@ -48,7 +48,7 @@ export default function GraphImage() {
           const timeDifference = lastDate - currentDate;
           const daysDifference = timeDifference / (1000 * 3600 * 24);
 
-          if (daysDifference < 1) {
+          if (daysDifference < 6) {
             count += 1;
           }
         });
